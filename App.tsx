88b7a112e968b9f7cd9698b03e8549460545f8ec
mrDav <kangnidavidgabiam@esp.sn>
2023-02-23 18:47:49 +0000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@ant-design/react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button type='primary' >Test</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

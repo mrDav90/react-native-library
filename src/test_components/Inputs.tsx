87bdb6e32d5c824cab2b361e96a86c3d71dd39d6
mrDav <kangnidavidgabiam@esp.sn>
2023-03-06 16:05:33 +0000

    new updates

--- src/test_components/Inputs.tsx
+++ src/test_components/Inputs.tsx
@@ -1,9 +1,20 @@
 import { StyleSheet } from 'react-native'
-import React from 'react'
-import { View , ScrollView , Text, WingBlank, Input } from '../../components';
+import React, { useState } from 'react'
+import { View , ScrollView , Text, WingBlank, Input, Button } from '../../components';
+import EmojiPicker from 'rn-emoji-keyboard';
 
 const Inputs = () => {
-    
+    const [isOpen , setIsOpen] = useState(false);
+    const handlePick = (emojiObject: any) => {
+        console.log(emojiObject)
+        /* example emojiObject = {
+            "emoji": "❤️",
+            "name": "red heart",
+            "slug": "red_heart",
+            "unicode_version": "0.6",
+          }
+        */
+      }
     return (
         <ScrollView  style={styles.container}>
             <View>
@@ -11,6 +22,25 @@ const Inputs = () => {
                 <WingBlank>
                     <Input placeholder='Email' /*onChangeText={(value : any)=>{console.log(value)}}*/ prefixIconName='mail-outline' label='Email'  />
                     <Input prefixIconName='lock-closed-outline' label='Mot de passe' outline type='password' />    
+                </WingBlank>
+                <WingBlank>
+                    <Button
+                    onPress={()=>{setIsOpen(true)}}
+                    > Open Emoji</Button>
+                    <EmojiPicker onEmojiSelected={handlePick} open={isOpen} onClose={() => setIsOpen(false)} 
+                        theme={{
+                            backdrop: '#16161888',
+                            container: '#282829',
+                            header: '#fff',
+                            skinTonesContainer: '#252427',
+                            category: {
+                                icon: '#766dfc',
+                                iconActive: '#fff',
+                                container: '#252427',
+                                containerActive: '#766dfc',
+                            },
+                        }}
+                    />
                 </WingBlank>
             </View>
         </ScrollView>
